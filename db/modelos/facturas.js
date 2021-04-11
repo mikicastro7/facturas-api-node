80527e56e957bb4f2fe2cb4ed8216b853fdacc4b
Miquel Castro <castro-miquel@hotmail.com>
2021-04-11 17:54:20 +0200

    Se añade el model y base de datos con el paquete sequelize

---
+++ db/modelos/facturas.js
@@ -0,0 +1,39 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../db");
+
+const Factura = sequelize.define("Factura", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  numero: {
+    type: DataTypes.STRING(15),
+    allowNull: false
+  },
+  fecha: {
+    type: DataTypes.STRING(15),
+    allowNull: false,
+  },
+  vencimiento: {
+    type: DataTypes.STRING(15),
+  },
+  concepto: {
+    type: DataTypes.TEXT,
+  },
+  base: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  tipoIva: {
+    type: DataTypes.INTEGER(11),
+    allowNull: false,
+  },
+  abonada: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0
+  }
+});
+
+module.exports = Factura;
