cfe5ee24d77f371742142a3a9183e405b15cbb3a
Miquel Castro <castro-miquel@hotmail.com>
2021-04-14 21:18:34 +0200

    Controladores y rutas proyectos creadas

--- controladores/proyectosController.js
+++ controladores/proyectosController.js
@@ -9,18 +9,115 @@ const objectFilter = (query) => {
     resultado.$and = tecnologias.map(tecnologia => ({ tecnologias: tecnologia }));
   }
   if (query.vencidos) {
-
+    if (query.vencidos === "true") {
+      resultado.entrega = { $gt: new Date().getTime() };
+    } else if (query.vencidos === "false") {
+      resultado.entrega = { $lt: new Date().getTime() };
+    } else {
+      resultado.error = generaError("true o false vencidas", 400);
+    }
   }
   return resultado;
 };
 
 const getProyectos = async (query) => {
   const objetoFilter = objectFilter(query);
-  console.log(objetoFilter);
-  const proyectos = await Proyecto.find(objetoFilter);
+  const result = {
+    proyectos: null,
+    error: null
+  };
+  if (objetoFilter.error) {
+    result.error = objetoFilter.error;
+    return result;
+  }
+  const proyectos = Proyecto.find(objetoFilter);
+
+  if (query.ordenPor === "fecha" || query.ordenPor === "nombre") {
+    if (query.orden) {
+      const ordenPor = query.ordenPor;
+      proyectos.sort({ [ordenPor]: query.orden });
+    } else {
+      proyectos.sort(query.ordenPor);
+    }
+  } else {
+    result.error = generaError("ordena por fecha o nombre", 400);
+  }
+  if (query.nPorPagina) {
+    if (query.pagina) {
+      proyectos.skip(query.nPorPagina * query.pagina);
+    }
+    proyectos.limit(+query.nPorPagina);
+  }
+  result.proyectos = await proyectos;
+  return result;
+};
+
+const getProyectosNoParamsQuery = async (query) => {
+  const proyectos = Proyecto.find(query);
   return proyectos;
 };
 
+const getProyecto = async (id) => {
+  const proyecto = Proyecto.findById(id);
+  return proyecto;
+};
+
+const crearProyecto = async (nuevoProyecto) => {
+  const nuevoProyectoBD = await Proyecto.create(nuevoProyecto);
+
+  return nuevoProyectoBD;
+};
+
+const sustituirProyecto = async (id, proyecto) => {
+  const proyectoEncontrado = await Proyecto.findById(id);
+  let nuevoProyecto;
+
+  if (proyectoEncontrado) {
+    await proyectoEncontrado.updateOne(proyecto);
+    nuevoProyecto = proyecto;
+  } else {
+    nuevoProyecto = await crearProyecto(proyecto);
+  }
+
+  return nuevoProyecto;
+};
+
+const modificarProyecto = async (id, cambios) => {
+  const respuesta = {
+    proyecto: null,
+    error: null
+  };
+  const proyecto = await Proyecto.findByIdAndUpdate(id, cambios);
+  if (proyecto) {
+    respuesta.proyecto = proyecto;
+  } else {
+    const error = generaError("El proyecto no existe", 404);
+    respuesta.error = error;
+  }
+  return respuesta;
+};
+
+const eliminarProyecto = async (id) => {
+  const proyectoEncontrado = await Proyecto.findByIdAndDelete(id);
+  const respuesta = {
+    proyecto: null,
+    error: null
+  };
+  if (proyectoEncontrado) {
+    respuesta.proyecto = proyectoEncontrado;
+  } else {
+    const error = generaError("El proyecto solicitado no existe", 404);
+    respuesta.error = error;
+  }
+  return respuesta;
+};
+
 module.exports = {
-  getProyectos
+  getProyectos,
+  getProyectosNoParamsQuery,
+  getProyecto,
+  crearProyecto,
+  sustituirProyecto,
+  modificarProyecto,
+  eliminarProyecto
 };
--- rutas/proyectos.js
+++ rutas/proyectos.js
@@ -1,12 +1,70 @@
 const express = require("express");
 // const { checkSchema, check } = require("express-validator");
-const { getProyectos } = require("../controladores/proyectosController");
+const {
+  getProyectos, getProyectosNoParamsQuery, getProyecto, crearProyecto, sustituirProyecto, modificarProyecto, eliminarProyecto
+} = require("../controladores/proyectosController");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const listaProyectos = await getProyectos(req.query);
-  res.json(listaProyectos);
+  const { proyectos, error } = await getProyectos(req.query);
+  if (error) {
+    next(error);
+  } else {
+    res.json(proyectos);
+  }
+});
+
+router.get("/pendientes", async (req, res, next) => {
+  const proyectos = await getProyectosNoParamsQuery({ estado: "pendiente" });
+  res.json(proyectos);
+});
+
+router.get("/en-progreso", async (req, res, next) => {
+  const proyectos = await getProyectosNoParamsQuery({ estado: "wip" });
+  res.json(proyectos);
+});
+
+router.get("/finalizados", async (req, res, next) => {
+  const proyectos = await getProyectosNoParamsQuery({ estado: "finalizado" });
+  res.json(proyectos);
+});
+
+router.get("/proyecto/:idProyecto", async (req, res, next) => {
+  const id = req.params.idProyecto;
+  const proyecto = await getProyecto(id);
+  res.json(proyecto);
+});
+
+router.post("/proyecto", async (req, res, next) => {
+  const proyecto = await crearProyecto(req.body);
+  res.json(proyecto);
+});
+
+router.put("/proyecto/:idProyecto", async (req, res, next) => {
+  const id = req.params.idProyecto;
+  const proyecto = await sustituirProyecto(id, req.body);
+  res.json(proyecto);
+});
+
+router.patch("/proyecto/:idProyecto", async (req, res, next) => {
+  const id = req.params.idProyecto;
+  const { proyecto, error } = await modificarProyecto(id, req.body);
+  if (error) {
+    next(error);
+  } else {
+    res.json(proyecto);
+  }
+});
+
+router.delete("/proyecto/:idProyecto", async (req, res, next) => {
+  const id = req.params.idProyecto;
+  const { proyecto, error } = await eliminarProyecto(id);
+  if (error) {
+    next(error);
+  } else {
+    res.json(proyecto);
+  }
 });
 
 module.exports = router;
